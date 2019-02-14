20a72c0e7c2d3cda89012bdb71a7a86289e4cb6e
BekzatBekturgan <47228030+BekzatBekturgan@users.noreply.github.com>
2019-02-15 00:34:59 +0600

    upd

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+
+    class Program
+    {
+        static bool IsPrime(int x) // to create a function to find the prime numbers 
+        {
+            if (x == 1) return false; // 1 is not a prime number,
+            for (int i = x - 1; i > 1; i--) // 
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // create variable int, then to add elements to int, convert string to int
+            int[] a = new int[n]; // new array with size n
+            string s = Console.ReadLine(); // write the numbers to the string
+            string[] parts = s.Split(); // to the new array parts write string s, and remove space with spilit
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(parts[i]); // in the array parts converts string to int array 
+            }
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (IsPrime(a[i]) == true) ans++; // use function to check number to the prime, and the quantity of prime numbers write to the ans
+                else a[i] = -1; // else to the element of array write -1, 
+            }
+            Console.WriteLine(ans); // output the quantity of primes number in array
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] != -1) // if element in array is equal to -1, then this elements is not prime, because of that program output the primes numbers
+                    Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // to create a new variable int, then input string, then convert string to the int
+            string q = "[*]";
+            string[,] s = new string[n, n]; // new 2-array with size n*n
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j <= i) s[i, j] = q; // write to the elements string q by the if
+                    else s[i, j] = ""; // else write nothing to place of elements in the array
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(s[i, j]); // output elements of 2 array
+                }
+                Console.WriteLine(); // the cursor to the new line
+            }
+        }
+    }
+}
